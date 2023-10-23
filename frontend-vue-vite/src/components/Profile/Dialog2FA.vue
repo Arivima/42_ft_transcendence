e7de628e509aaa5ea66439bb673803b1dee0d777
Earendil <marinelli.matteo.rhcp@gmail.com>
2023-10-23 19:36:20 +0200

    App - frontend: Dialog2FA.vue DEBUG -- functional

--- frontend-vue-vite/src/components/Profile/Dialog2FA.vue
+++ frontend-vue-vite/src/components/Profile/Dialog2FA.vue
@@ -4,14 +4,14 @@ import { VOtpInput } from 'vuetify/labs/VOtpInput'
 import axios from 'axios'
 
 export default defineComponent({
-	components : {
-		VOtpInput,
+	components: {
+		VOtpInput
 	},
 	props: {
 		mode: {
 			type: String,
 			required: true,
-			validator(value : string) {
+			validator(value: string) {
 				return ['enable', 'login', 'disable'].includes(value)
 			}
 		}
@@ -26,15 +26,15 @@ export default defineComponent({
 			snackMessage: '',
 			errorPopUp: false,
 			errorMessage: '',
-			loading: false,
+			loading: false
 		}
 	},
-	methods : {
-		displaySnackbar(message : string) {
+	methods: {
+		displaySnackbar(message: string) {
 			this.snackbar = true
 			this.snackMessage = message
 		},
-		displayError(message : string) {
+		displayError(message: string) {
 			this.errorPopUp = true
 			this.errorMessage = message
 		},
@@ -44,137 +44,142 @@ export default defineComponent({
 				this.qrCode = (await axios.get(`auth/2fa/qrcode`)).data.qrcode
 				this.loading = false
 			} catch (error) {
-				console.log(error);
+				console.log(error)
 			}
 		},
 		async sendOtp() {
 			this.loading = true
-			switch(this.mode) {
-				case 'enable' : {
+			switch (this.mode) {
+				case 'enable': {
 					axios
 						.post(`auth/2fa/`)
-						.then(response => {
-							if (response.data === true){
-								this.displaySnackbar("Confirmed authentication. 2 factor authentication has been enabled. Proceeding to app")
+						.then((response) => {
+							if (response.data === true) {
+								this.displaySnackbar(
+									'Confirmed authentication. 2 factor authentication has been enabled. Proceeding to app'
+								)
 								this.dialogBox = false
-							}
-							else
-								this.displayError("Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.")
+							} else
+								this.displayError(
+									'Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.'
+								)
 						})
-						.catch(error => {
-							this.displayError("Error: Server internal error.")
+						.catch((error) => {
+							this.displayError('Error: Server internal error.')
 							console.log(error)
 						})
-						.finally(() => this.loading = false)
+						.finally(() => (this.loading = false))
 					break
 				}
-				case 'login' : {
+				case 'login': {
 					axios
 						.post(`auth/2fa/login`)
-						.then(response => {
-							if (response.data === true){
-								this.displaySnackbar("Confirmed authentication. Proceeding to app")
+						.then((response) => {
+							if (response.data === true) {
+								this.displaySnackbar('Confirmed authentication. Proceeding to app')
 								this.dialogBox = false
-							}
-							else
-								this.displayError("Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.")
+							} else
+								this.displayError(
+									'Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.'
+								)
 						})
-						.catch(error => {
-							this.displayError("Error: Server internal error.")
+						.catch((error) => {
+							this.displayError('Error: Server internal error.')
 							console.log(error)
 						})
-						.finally(() => this.loading = false)
+						.finally(() => (this.loading = false))
 					break
 				}
-				case 'disable' : {
+				case 'disable': {
 					axios
 						.delete(`auth/2fa/remove`)
-						.then(response => {
-							if (response.data === true){
-								this.displaySnackbar("Confirmed authentication. 2 factor authentication has been disabled")
+						.then((response) => {
+							if (response.data === true) {
+								this.displaySnackbar(
+									'Confirmed authentication. 2 factor authentication has been disabled'
+								)
 								this.dialogBox = false
-							}
-							else
-								this.displayError("Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.")
+							} else
+								this.displayError(
+									'Error: Invalid one time password. Please retry. To generate a new QR code close this window and try again.'
+								)
 						})
-						.catch(error => {
-							this.displayError("Error: Server internal error.")
+						.catch((error) => {
+							this.displayError('Error: Server internal error.')
 							console.log(error)
 						})
-						.finally(() => this.loading = false)
+						.finally(() => (this.loading = false))
 					break
 				}
 				default: {
 					break
 				}
 			}
-		},
+		}
 	},
 	watch: {
-		dialogBox(isActive : boolean){
+		dialogBox(isActive: boolean) {
 			if (isActive == true) {
-				switch(this.mode) {
-					case 'enable' : {
-						this.title = "Enable 2 factor authentication"
+				switch (this.mode) {
+					case 'enable': {
+						this.title = 'Enable 2 factor authentication'
 						this.getQRcode()
 						break
 					}
-					case 'login' : {
-						this.title = "Confirm login with 2 factor authentication"
+					case 'login': {
+						this.title = 'Confirm login with 2 factor authentication'
 						break
 					}
-					case 'disable' : {
-						this.title = "Disable 2 factor authentication"
+					case 'disable': {
+						this.title = 'Disable 2 factor authentication'
 						break
 					}
 					default:
 						break
 				}
-			}
-			else {
-				this.otp = '',
-				this.qrCode = '',
-				this.title = '',
-				this.errorMessage = '',
-				this.snackMessage = '',
-				this.loading = false,
+			} else {
+				this.otp = ''
+				this.qrCode = ''
+				this.title = ''
+				this.errorMessage = ''
+				this.snackMessage = ''
+				this.loading = false
 			}
 		}
 	},
-	mounted() {
-		
-	}
+	mounted() {}
 })
 </script>
 
-
-
 <template>
-	<v-dialog
-		v-model="dialogBox"
-		activator="parent"
-	>
-		<v-card
-			:title=title
-			rounded
-			class="dialog bg-white ma-auto pa-4"
-		>
+	<v-dialog v-model="dialogBox" activator="parent">
+		<v-card :title="title" rounded class="dialog bg-white ma-auto pa-4">
 			<v-card-item v-if="qrCode">
 				<h3 class="text-overline">QR code</h3>
-				<h4 class="font-weight-light">Please scan this QR code in your authentication app</h4>
+				<h4 class="font-weight-light">
+					Please scan this QR code in your authentication app
+				</h4>
 				<div class="align-center justify-center ma-4 d-flex">
 					<v-img
 						class="bg-white border"
 						max-width="300"
 						aspect-ratio="1"
-						:src=qrCode
+						:src="qrCode"
 					></v-img>
 				</div>
 			</v-card-item>
 			<v-card-item>
 				<h3 class="text-overline">Verification Code</h3>
-				<h4 class="font-weight-light">Please enter the verification code from your authentication app</h4>
-				<v-alert class="font-weight-light my-2 pa-2" density="compact" color="purple" v-show="errorMessage">{{ errorMessage }}</v-alert>
+				<h4 class="font-weight-light">
+					Please enter the verification code from your authentication app
+				</h4>
+				<v-alert
+					class="font-weight-light my-2 pa-2"
+					density="compact"
+					color="purple"
+					v-show="errorMessage"
+					>{{ errorMessage }}</v-alert
+				>
 				<v-otp-input
 					v-model="otp"
 					class="ma-2"
@@ -189,42 +194,32 @@ export default defineComponent({
 					class="my-2"
 					color="grey"
 					variant="text"
-					@click="otp=''"
+					@click="otp = ''"
 				></v-btn>
 			</v-card-item>
-			<div class="text-end ">
+			<div class="text-end">
 				<v-btn
 					@click="dialogBox = false"
 					border
-					class="me-4 "
+					class="me-4"
 					color="grey"
 					text="Cancel"
 					variant="text"
 				></v-btn>
 			</div>
 
+			<!-- :timeout="5000" -->
+			<v-snackbar v-model="snackbar" color="success" multi-line location="top">
+				{{ snackMessage }}
 
-				<!-- :timeout="5000" -->
-			<v-snackbar
-				v-model="snackbar"
-				color="success"
-				multi-line
-				location="top"
-			>
-			{{ snackMessage }}
-
-			<template v-slot:actions>
+				<template v-slot:actions>
 					<!-- color="blue" -->
 					<!-- variant="text" -->
-				<v-btn
-					@click="snackbar = false"
-				>
-				Close
-				</v-btn>
-			</template>
+					<v-btn @click="snackbar = false"> Close </v-btn>
+				</template>
 			</v-snackbar>
 
-				<!-- :timeout="10000" -->
+			<!-- :timeout="10000" -->
 			<v-snackbar
 				v-model="errorPopUp"
 				color="red-darken-2"
@@ -233,17 +228,13 @@ export default defineComponent({
 				location="bottom"
 				location-strategy="connected"
 			>
-			{{ errorMessage }}
+				{{ errorMessage }}
 
-			<template v-slot:actions>
+				<template v-slot:actions>
 					<!-- color="blue" -->
 					<!-- variant="text" -->
-				<v-btn
-					@click="errorPopUp = false"
-				>
-				Close
-				</v-btn>
-			</template>
+					<v-btn @click="errorPopUp = false"> Close </v-btn>
+				</template>
 			</v-snackbar>
 		</v-card>
 	</v-dialog>
