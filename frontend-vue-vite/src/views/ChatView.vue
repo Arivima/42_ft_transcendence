8d4274b9122efbd0793dbd675e27a79a5ac117a4
Arielle Villa-massone <avilla-m@e4r13p4.42roma.it>
2023-11-22 17:32:58 +0100

    Chat - Added component DialogWaiting, DialogInvite, Notifications

--- frontend-vue-vite/src/views/ChatView.vue
+++ frontend-vue-vite/src/views/ChatView.vue
@@ -2,12 +2,17 @@
 <script setup lang="ts">
 import NavSideBar from '../components/NavSideBar.vue'
 import Chat from '../components/Chat/Chat.vue'
+import DialogWaiting from '@/components/Game/DialogWaiting.vue';
+import DialogInvite from '@/components/Game/DialogInvite.vue';
+import Notifications from '@/components/Notifications.vue';
 </script>
 
 <template>
     <div class="chat">
         <NavSideBar/>
-
+        <Notifications></Notifications>
+        <DialogWaiting></DialogWaiting>
+        <DialogInvite></DialogInvite>
         <Chat/>
         <!-- <v-navigation-drawer permanent>
         <v-list>
