5929c3382aa9e0ef84967440a4753a70d309f1c9
ripa <davide.ripanucci@gmail.com>
2023-11-28 02:13:38 +0100

    fix

--- frontend-vue-vite/src/components/Chat/GroupInfoDialog.vue
+++ frontend-vue-vite/src/components/Chat/GroupInfoDialog.vue
@@ -2,9 +2,7 @@
 	<v-dialog v-model="groupInfoDialog" max-width="600">
 		<v-card>
 			<div class="d-flex justify-space-between align-center pa-2 pointer elevation-1 mb-2">
-				<!-- <v-avatar class="mr-2"> -->
 				<v-card-title>{{ groupInfo.name }}</v-card-title>
-				<!-- edit group visibility button -->
 			<div>
 				<v-btn icon @click="editGroupPrivacy()" v-if="this.user.isAdmin" class="mr-2">
 					<v-icon>mdi-pencil</v-icon>
@@ -15,8 +13,6 @@
 			</div>
 			</div>
 			<v-card-text>
-				<!-- <v-img :src="groupInfo.imageUrl" aspect-ratio="1.5">
-		  </v-img> -->
 				<h3>Members</h3>
 				<v-list dense>
 					<div class="flex-1 mt-4 mb-4 overflow-y-auto fill-height max-height-400 px-4">
